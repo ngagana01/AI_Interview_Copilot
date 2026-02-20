d3ddc9702f1f11710bf6cc0055d822380f863f3b
Gagana N <ngagana01@gmail.com>
2026-02-20 12:49:49 +0530

    Update resume.py

--- backend/resume.py
+++ backend/resume.py
@@ -1,18 +1,8 @@
 import PyPDF2
 
 def parse_resume(file):
-
     reader=PyPDF2.PdfReader(file)
     text=""
-
     for page in reader.pages:
         text+=page.extract_text()
-
-    skills=[]
-    keywords=["python","machine learning","ai","sql","react","data"]
-
-    for k in keywords:
-        if k in text.lower():
-            skills.append(k)
-
-    return {"skills":skills,"score":len(skills)*15}
+    return text
